6e6de6c8dddb4c29b3c4c251d3a19cb29cd6df25
joshuahill <joshdh81691@gmail.com>
2022-12-15 08:29:25 +0000

    Answer puzzle 2 part 2

--- puzzle-2/Josh/puzzle-2.py
+++ puzzle-2/Josh/puzzle-2.py
@@ -18,3 +18,25 @@ with open('input.txt') as input_file:
         score = score + outcome_score[player][opponent]
 
 print("final score:", score)
+
+# PART TWO:
+# For some reason it massively feels like I'm cheating with these maps 🤔
+
+# lose, draw, win
+outcome_score = {'X': 0, 'Y': 3, 'Z': 6}
+
+# Rock: lose via scissors, draw via rock, win via paper etc.
+play_score = {
+    'A': {'X': 3, 'Y': 1, 'Z': 2},
+    'B': {'X': 1, 'Y': 2, 'Z': 3},
+    'C': {'X': 2, 'Y': 3, 'Z': 1},
+}
+
+with open('input.txt') as input_file:
+    score = 0
+    for line in input_file:
+        opponent, desired_outcome = line[:3].split(' ')  # first three chars to avoid newline \n
+        score = score + outcome_score[desired_outcome]
+        score = score + play_score[opponent][desired_outcome]
+
+print("part two score", score)
